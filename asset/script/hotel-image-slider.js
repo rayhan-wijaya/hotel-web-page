65e4fdfd32dfa4fe64db1810b5515d5d4874eaa7
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-14 23:58:20 +0700

    Defined variables for hotel-image-slider.js

---
+++ asset/script/hotel-image-slider.js
@@ -0,0 +1,13 @@
+let currentImageIndex = 0;
+
+const minimumImageIndex = 0;
+const maximumImageIndex = 4;
+
+const imageContainerId = "hotel-image-container";
+const imageContainer = document.getElementById(imageContainerId);
+
+const imageNextButtonId = "hotel-image-next";
+const imageNextButton = document.getElementById(imageNextButtonId);
+
+const imagePrevButtonId = "hotel-image-prev";
+const imagePrevButton = document.getElementById(imagePrevButtonId);
