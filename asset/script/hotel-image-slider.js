7c70a7333f4b7c4cf06c7176a60fe89bd0958b6f
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-14 23:58:54 +0700

    Created image slider handlers

--- asset/script/hotel-image-slider.js
+++ asset/script/hotel-image-slider.js
@@ -11,3 +11,34 @@ const imageNextButton = document.getElementById(imageNextButtonId);
 
 const imagePrevButtonId = "hotel-image-prev";
 const imagePrevButton = document.getElementById(imagePrevButtonId);
+
+const handleNextImage = () => {
+  if ((currentImageIndex + 1) > maximumImageIndex) {
+    currentImageIndex = minimumImageIndex;
+
+    return;
+  }
+
+  ++currentImageIndex;
+};
+
+const handlePrevImage = () => {
+  if ((currentImageIndex - 1) < minimumImageIndex) {
+    currentImageIndex = maximumImageIndex;
+
+    return;
+  }
+
+  --currentImageIndex;
+};
+
+const main = () => {
+  if (!imageContainer || !imageNextButton || !imagePrevButton) {
+    return;
+  }
+
+  imageNextButton.onclick = handleNextImage;
+  imagePrevButton.onclick = handlePrevImage;
+};
+
+main();
