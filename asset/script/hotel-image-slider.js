e8f8dbf650c887b13e251a9c669fc91002a7bd0a
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-15 08:54:25 +0700

    Created updateImage function

--- asset/script/hotel-image-slider.js
+++ asset/script/hotel-image-slider.js
@@ -12,6 +12,19 @@ const imagePrevButton = document.getElementById(imagePrevButtonId);
 const minimumImageIndex = 0;
 const maximumImageIndex = imageContainer.childElementCount - 1 ?? null;
 
+/**
+  * @param {number} newImageIndex
+  */
+const updateImage = (newImageIndex) => {
+  const newImage = imageContainer.children[newImageIndex];
+  newImage.classList.remove("hidden");
+
+  const currentImage = imageContainer.children[currentImageIndex];
+  currentImage.classList.add("hidden");
+
+  currentImageIndex = newImageIndex;
+}
+
 const handleNextImage = () => {
   if (!maximumImageIndex) {
     return;
