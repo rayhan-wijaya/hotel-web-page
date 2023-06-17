54e20386fded54a0e370d3108bbf9c4ac39917a7
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-17 11:52:05 +0700

    Created whatsapp colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "whatsapp-500": "#32d951",
+        "whatsapp-600": "#1c9732",
+      },
+    },
   },
   plugins: [
     require("tailwindcss"),
